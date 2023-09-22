1d0a275af9fcee7f761c502b54e97911c97b9979
DESKTOP-7EH22KF\L5-Alumno 12 <carlaz020400@gmail.com>
2023-09-22 09:00:07 -0600

    wallJump

--- Assets/Scripts/player_Movement.cs
+++ Assets/Scripts/player_Movement.cs
@@ -1,25 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class player_Movement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    [SerializeField] private float radius;
     //private float moveSpeed = 5f;
+
+    public float horizontal;
+    public bool isFacingright = true;
+
     [Header("Jump")]
     [SerializeField] private float jumpSpeed;
     [SerializeField] private bool canJump;
+    [SerializeField] private float speed;
+
 
     [Header("Ground")]
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private bool isGrounded;
+
     [Header("Wall")]
     [SerializeField] private Transform wallCheck;
     [SerializeField] private LayerMask wallLayer;
     [SerializeField] private bool isTouchingWall;
 
-    [SerializeField] private float radius;
+    [Header("WallSliding & Wall Jumping")]
+    [SerializeField] private bool isWallSliding;
+    [SerializeField] private float wallSlidingSpeed;
+    [SerializeField] private bool isWallJumping;
+    private float wallJumpingDirection;
+    [SerializeField] private float wallJumpingTime =.2f;
+    [SerializeField] private float wallJumpingCounter;
+    [SerializeField] private float wallJumpingDuration = .4f;
+    [SerializeField] private Vector2 wallJumpingPower = new Vector2(0f, 16f);
+
+
+
+
 
     private void Start()
     {
@@ -41,7 +62,23 @@ public class player_Movement : MonoBehaviour
         {
             Jump();
         }
+
+        WallSlide();
+        WallJump();
+
+        if (!isWallJumping)
+        {
+            Flip();
+        }
     }
+    private void FixedUpdate()
+    {
+        if (!isWallJumping)
+        {
+            rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        }
+    }
+
 
     public void Jump()
     {
@@ -53,5 +90,68 @@ public class player_Movement : MonoBehaviour
     {
         Gizmos.DrawWireSphere(groundCheck.position, radius);
         Gizmos.DrawWireSphere(wallCheck.position, radius);
+    }
+
+    private void  WallSlide()
+    {
+        if (isTouchingWall && !isGrounded)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+    
+        }
+        else
+        {
+            isWallSliding = false;
+
+        }
+        
+    }
+    private void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+        if ( Input.GetKeyDown(KeyCode.Space) && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDuration * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+            
+            if(transform.localScale.x != wallJumpingDirection)
+            {
+                isFacingright = !isFacingright;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+        Invoke(nameof(StopWallJumping), wallJumpingDirection);
+        
+        }
+    }
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+    private void Flip()
+    {
+        if(isFacingright && horizontal <0f || !isFacingright && horizontal > 0f)
+        {
+            isFacingright = !isFacingright;
+            Vector3 localscale = transform.localScale;
+            localscale.x *= -1f;
+            transform.localScale= localscale;
+        }
     }
 }
